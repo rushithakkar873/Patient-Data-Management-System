4dd1c72ef0efed85c08e67dc206f8cb2d88e1619
Ruc08 <ructhakkar123@gmail.com>
2024-03-01 14:05:37 +0530

    Completed frontend setup

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+const App = () => {
+
+  return (
+    <>
+      <Button>
+        Click me
+      </Button>
+    </>
+  )
+}
+
+export default App
